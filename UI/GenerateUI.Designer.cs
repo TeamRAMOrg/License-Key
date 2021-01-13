b226191ba075b41380b83524ad56f76a2aed34a3
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-13 14:48:13 +0600

    Generate UI design

--- UI/GenerateUI.Designer.cs
+++ UI/GenerateUI.Designer.cs
@@ -29,23 +29,130 @@ namespace LicenseKey
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExpDays = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtProductKey = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.cmbLicenseType = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product ID:";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(112, 39);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(281, 20);
+            this.txtProductID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "License Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Exp Days:";
+            // 
+            // txtExpDays
+            // 
+            this.txtExpDays.Location = new System.Drawing.Point(112, 91);
+            this.txtExpDays.Name = "txtExpDays";
+            this.txtExpDays.Size = new System.Drawing.Size(116, 20);
+            this.txtExpDays.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Product Key:";
+            // 
+            // txtProductKey
+            // 
+            this.txtProductKey.Location = new System.Drawing.Point(112, 117);
+            this.txtProductKey.Name = "txtProductKey";
+            this.txtProductKey.Size = new System.Drawing.Size(281, 20);
+            this.txtProductKey.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(318, 154);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // cmbLicenseType
+            // 
+            this.cmbLicenseType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLicenseType.FormattingEnabled = true;
+            this.cmbLicenseType.Items.AddRange(new object[] {
+            "Full"});
+            this.cmbLicenseType.Location = new System.Drawing.Point(112, 64);
+            this.cmbLicenseType.Name = "cmbLicenseType";
+            this.cmbLicenseType.Size = new System.Drawing.Size(116, 21);
+            this.cmbLicenseType.TabIndex = 3;
             // 
             // GenerateUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(437, 215);
+            this.Controls.Add(this.cmbLicenseType);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtProductKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExpDays);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "GenerateUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Generate";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtExpDays;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtProductKey;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ComboBox cmbLicenseType;
     }
 }
 
--- UI/GenerateUI.cs
+++ UI/GenerateUI.cs
@@ -16,5 +16,10 @@ namespace LicenseKey
         {
             InitializeComponent();
         }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
