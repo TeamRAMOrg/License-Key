bff107e136c86d3ee9f2feb56dd717f5b65c5d7c
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-13 14:51:25 +0600

    Registration UI design

--- UI/RegistrationUI.Designer.cs
+++ UI/RegistrationUI.Designer.cs
@@ -29,22 +29,81 @@ namespace LicenseKey.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtProductKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(297, 79);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 11;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // txtProductKey
+            // 
+            this.txtProductKey.Location = new System.Drawing.Point(91, 53);
+            this.txtProductKey.Name = "txtProductKey";
+            this.txtProductKey.Size = new System.Drawing.Size(281, 20);
+            this.txtProductKey.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Product Key:";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(91, 27);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(281, 20);
+            this.txtProductID.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Product ID:";
             // 
             // RegistrationUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 337);
+            this.ClientSize = new System.Drawing.Size(387, 119);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtProductKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "RegistrationUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registration";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtProductKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.Label label1;
     }
 }
--- UI/RegistrationUI.cs
+++ UI/RegistrationUI.cs
@@ -16,5 +16,10 @@ namespace LicenseKey.UI
         {
             InitializeComponent();
         }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
